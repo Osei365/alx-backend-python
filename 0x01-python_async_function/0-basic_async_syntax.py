af4fa9e9b8f76ea24b4d28b9753781cf3e5103b4
Osei <williamsabodunrin@gmail.com>
2024-01-16 00:53:32 +0100

    Update 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,8 +6,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
-    '''creates a corountine'''
+async def wait_random(max_delay: int = 10) -> float:
+    '''creates a corountine''' 
     r = random.uniform(0, max_delay)
     await asyncio.sleep(r)
     return r
