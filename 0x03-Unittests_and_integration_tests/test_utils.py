13d5c24c9315b6c7d88ab04155892f275fc5c59c
Osei <williamsabodunrin@gmail.com>
2024-02-06 00:51:49 +0000

    patching amd mocking

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@
 import utils
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -24,3 +25,20 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path, expected):
         with self.assertRaises(KeyError):
             utils.access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """test get jason."""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, payload):
+        """ testing the utils get json method."""
+        config = {'return_value.json.return_value': payload}
+        patcher = patch('requests.get', **config)
+        mockClass = patcher.start()
+        self.assertEqual(utils.get_json(url), payload)
+        mockClass.assert_called_once()
+        patcher.stop()
