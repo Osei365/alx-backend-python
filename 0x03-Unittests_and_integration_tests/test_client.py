133d060d6f0cbce98c36a131a1554a74214b85d3
Osei <williamsabodunrin@gmail.com>
2024-02-06 02:44:30 +0000

    property mock

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -50,3 +50,13 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(result, check)
             mock.assert_called_once()
             mock_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """tests for license."""
+        instance = GithubOrgClient('testing')
+        result = instance.has_license(repo, license_key)
+        self.assertEqual(result, expected)
